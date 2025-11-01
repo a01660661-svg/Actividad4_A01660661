ea64870dd49f0340da5ca8f8db93fda757af8c6c
Andrea <A01660661@tec.mx>
2025-10-31 18:35:49 -0600

    Si se aplica, correcion de errores de identacion, importaciones y uso de funciones

--- snake.py
+++ snake.py
@@ -1,18 +1,6 @@
-"""Snake, classic arcade game.
-
-Exercises
-
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to mouse clicks.
-"""
-
 from random import randrange
-from turtle import *
-
+import turtle
 from freegames import square, vector
-
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
@@ -36,7 +24,7 @@ def move():
 
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
-        update()
+        turtle.update()
         return
 
     snake.append(head)
@@ -48,23 +36,23 @@ def move():
     else:
         snake.pop(0)
 
-    clear()
+    turtle.clear()
 
     for body in snake:
         square(body.x, body.y, 9, 'black')
 
     square(food.x, food.y, 9, 'green')
-    update()
-    ontimer(move, 100)
+    turtle.update()
+    turtle.ontimer(move, 100)
 
 
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
-listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
+turtle.setup(420, 420, 370, 0)
+turtle.hideturtle()
+turtle.tracer(False)
+turtle.listen()
+turtle.onkey(lambda: change(10, 0), 'Right')
+turtle.onkey(lambda: change(-10, 0), 'Left')
+turtle.onkey(lambda: change(0, 10), 'Up')
+turtle.onkey(lambda: change(0, -10), 'Down')
 move()
-done()
+turtle.done()
